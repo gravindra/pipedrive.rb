786582f4fec86037f8e5ff697fe333c7016a47f7
gravindra <ravighare26@gmail.com>
2017-02-23 19:28:48 +0530

    adds find by email option without pagination support

--- lib/pipedrive/utils.rb
+++ lib/pipedrive/utils.rb
@@ -14,5 +14,10 @@ module Pipedrive
         start = res.try(:additional_data).try(:pagination).try(:next_start)
       end
     end
+
+    def unfollow_pagination(method, args, params)
+      start = params[:start] || 0
+      res = __send__(method, *args, params.merge(start: start))
+    end
   end
 end
